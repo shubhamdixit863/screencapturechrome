4b06eaa62f800746e77d06db573809b03da9e637
shubham <shubhamdixit863@gmail.com>
2023-03-27 16:24:47 +0530

    screencapture

--- google.js
+++ google.js
@@ -115,7 +115,7 @@ async function uploadFile() {
 	form.append('file', file);
 
 	var xhr = new XMLHttpRequest();
-	xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&upload_id=xa298sd_sdlkj2');
+	xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&upload_id=xa298sd_sdlkj2');
 	xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
 	xhr.responseType = 'json';
 	xhr.onload = () => {
